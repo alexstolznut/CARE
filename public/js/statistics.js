fcbd2a3af94ac21e376cfe8a1681ef5c03d8fb98
Vilan Le <v3le@ucsd.edu>
2016-05-28 23:13:38 -0700

    two data sets added to stats page

--- public/js/statistics.js
+++ public/js/statistics.js
@@ -35,12 +35,59 @@ $.getJSON("/vamentalhealth.json", function (data) {
 
     });
     console.log(total2/itemCount);
-
-    renderChart([total, total2]);
+    names = [uniquevets,propo];
+    pieChart([total2/itemCount, 100-total2/itemCount], names);
+    barChart(total);
 });
 
-function renderChart(totals) {
+function pieChart(totals, names) {
     var data=totals;
+
+    var width = 960,
+        height = 500,
+        radius = Math.min(width, height) / 2;
+
+    var color = d3.scale.ordinal()
+        .range(["#98abc5", "#8a89a6"]);
+
+    var arc = d3.svg.arc()
+        .outerRadius(radius - 10)
+        .innerRadius(0);
+
+    var labelArc = d3.svg.arc()
+        .outerRadius(radius - 40)
+        .innerRadius(radius - 40);
+
+    var pie = d3.layout.pie()
+        .sort(null)
+        .value(function(d) { return d; });
+
+    var svg = d3.select(".pie").append("svg")
+        .attr("width", width)
+        .attr("height", height)
+      .append("g")
+        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+      var g = svg.selectAll(".arc")
+          .data(pie(data))
+        .enter().append("g")
+          .attr("class", "arc");
+
+      g.append("path")
+          .attr("d", arc)
+          .style("fill", function(d) { return color(d.data); });
+
+      g.append("text")
+          .attr("transform", function(d) { return "translate(" + labelArc.centroid(d) + ")"; })
+          .attr("dy", ".35em")
+          .text(function(d) { return d.data; });
+
+
+}
+
+
+function barChart(totals) {
+    var data=[totals];
 
 
     var x = d3.scale.linear()
@@ -53,19 +100,4 @@ function renderChart(totals) {
           .enter().append("div")
             .style("width", function(d) { return x(d) + "px"; })
             .text(function(d) { return d; });
-
-
-    // d3.select(".chart")
-    //   .selectAll("div")
-    //     .data(data)
-    //   .enter().append("div")
-    //     .style("width", function(d) { return x(d[0]) + "px"; })
-    //     .text(function(d) { return d[0]; });
-
-    // var yTextPadding = 20;
-
-
-
 }
-
-
