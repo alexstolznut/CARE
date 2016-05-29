2250305868e7a8ba50359d93cb45e5f464becd9d
Vilan Le <v3le@ucsd.edu>
2016-05-28 22:49:59 -0700

    added one more data set in stats page

--- public/js/statistics.js
+++ public/js/statistics.js
@@ -1,6 +1,9 @@
 
 var total = 0;
-
+var total2 = 0;
+var itemCount = 0;
+var uniquevets;
+var propo;
 
 "use strict";
 $.getJSON("/vamentalhealth.json", function (data) {
@@ -10,28 +13,59 @@ $.getJSON("/vamentalhealth.json", function (data) {
 
         // Get all the categories
         var items = this.Item;
+        // console.log(items);
         if (items == "Unique Veterans Seen In Inpatient Mental Health") {
             var value = this.Value;
             total = parseInt(value) + total;
+            uniquevets = items;
+        }
+        if(items === "Proportion of Veterans with Possible Mental Illness Seen in Any Mental Health"){
+            propo = items;
+            var val = this.Value;
+            var number = "";
+
+            for (var i = 0, len = val.length; i < len-1; i++) {
+              number=number+val[i];
+            }
+            console.log(number);
+
+            total2 = parseInt(number) + total2;
+            itemCount++;
         }
 
     });
-    renderChart(total);
+    console.log(total2/itemCount);
+
+    renderChart([total, total2]);
 });
 
-function renderChart(total) {
-    var data=[total];
+function renderChart(totals) {
+    var data=totals;
+
 
     var x = d3.scale.linear()
         .domain([0, d3.max(data)])
         .range([0, 420]);
 
-    d3.select(".chart")
-      .selectAll("div")
-        .data(data)
-      .enter().append("div")
-        .style("width", function(d) { return x(d) + "px"; })
-        .text(function(d) { return d; });
+        d3.select(".chart")
+          .selectAll("div")
+            .data(data)
+          .enter().append("div")
+            .style("width", function(d) { return x(d) + "px"; })
+            .text(function(d) { return d; });
+
+
+    // d3.select(".chart")
+    //   .selectAll("div")
+    //     .data(data)
+    //   .enter().append("div")
+    //     .style("width", function(d) { return x(d[0]) + "px"; })
+    //     .text(function(d) { return d[0]; });
+
+    // var yTextPadding = 20;
+
+
+
 }
 
 
